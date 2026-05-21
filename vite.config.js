1b821a598903cb271ab87a1dc09f67a54975bbfe
kraftkartel <kraftkartel20@gmail.com>
2026-05-21 07:53:36 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ['fsevents']
+  },
   build: {
     rollupOptions: {
       external: ['fsevents']
